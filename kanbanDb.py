c69b8b9b6d8bd34cfaae4c5bd1b79cda6c1d71cf
Flevian <fleviankanaiza@gmail.com>
2017-03-15 16:15:09 +0300

    Update database design and add a collumn to hold task duration

--- kanbanDb.py
+++ kanbanDb.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+import time
  
 Base = declarative_base()
  
@@ -10,7 +11,9 @@ class KanbanDb(Base):
     # Each column is a normal Python instance attribute.
     id = Column(Integer, primary_key = True)
     task_name = Column(String(255), nullable = False)
-    task_status = Column(String(255), nullable = False)
+    task_status = Column(String(50), nullable = False)
+    start_time = Column(DateTime, nullable =True)
+    task_duration = Column(String(50), nullable = True)
  
 engine = create_engine('sqlite:///kanban.db') # Create an engine that stores data in the local directory's
 Base.metadata.create_all(engine) # Create table in the engine
