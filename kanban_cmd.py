fd4ca464bb8f8a8c691af12aacb6b4daf6115a5c
Flevian <fleviankanaiza@gmail.com>
2017-03-16 13:10:06 +0300

    make UI more appealing to the user

--- kanban_cmd.py
+++ kanban_cmd.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 """
-This example uses docopt with the built in cmd module to demonstrate an
-interactive command application.
+Kanban is an interactive command application.
 Usage:
-    kanban todo <taskname>
-    kanban list_todo
-    kanban doing <task_id>
-    kanban done <task_id>
-    kanban list_doing
-    kanban list_done
-    kanban list_all
-    kanban (-i | --interactive)
-    kanban (-h | --help | --version)
+    Todo todo <taskname>
+    Todo todo_list
+    Todo doing <task_id>
+    Todo done <task_id>
+    Todo doing_list
+    Todo done_list
+    Todo list_all
+    Todo sync_tasks
+    Todo (-i | --interactive)
+    Todo (-h | --help | --version)
 Options:
     -i, --interactive  Interactive Mode
     -h, --help  Show this screen and exit.
@@ -21,6 +21,8 @@ Options:
 import sys
 import cmd
 from docopt import docopt, DocoptExit
+from termcolor import colored
+from pyfiglet import Figlet
 from kanban import Kanban
 kanban = Kanban()
 
@@ -56,40 +58,77 @@ def docopt_cmd(func):
     return fn
 
 
+
 class ToDo(cmd.Cmd):
-    intro = 'Welcome to my interactive program!' \
-        + ' (type help for a list of commands.)'
-    prompt = 'todo > '
+    header = "  Design to help one to know task progress"
+    main_menu = "main menu"
+    main_option = "Menu Option 2"
+    instruction = "Instructions:"
+    todo_one = "Todo >> Input: task todo | view todo task list |"
+    todo_two = "| move task from todo to doing | view doing task list"
+    todo_three = "| move task from doing to done | view done task list"
+    todo_four = "| View all task | sync database to firebase"
+    todo_five = "Type help or -h for a list of commands"
+    print(colored("  " + "~" * 89,"white"))
+    print(colored("  " + Figlet(font='slant').renderText('\t      TODO TASK MANAGER'),"white"))
+    print(" " * 93)
+    print(" " * 93)
+    print(colored("  ~" + "~" * 89 + "~","blue"))
+    print(colored("  ~" + header.center(89, " ") + "~","white"))
+    print(colored("  ~" + "~" * 89 + "~","blue"))
+    print(colored("  ~" + instruction.ljust(89, " ") + "~","white"))
+    print(colored("  ~" + "~" * 89 + "~","white"))
+    print(colored("  ~" + main_menu.center(89, " ") + "~","green"))
+    print(colored("  ~" + "~" * 89 + "~","white"))
+    print(colored("  ~" + " " * 89 + "~","white"))
+    print(colored("  ~" + todo_one.ljust(89, " ") + "~","white"))
+    print(colored("  ~" + todo_two.center(89, " ") + "~","white"))
+    print(colored("  ~" + todo_three.center(89, " ") + "~","white"))
+    print(colored("  ~" + todo_four.center(89, " ") + "~","white"))
+    print(colored("  ~" + " " * 89 + "~","white"))
+    print(colored("  ~" + "~" * 89 + "~","white"))
+    print(colored("  ~" + main_option.center(89, " ") + "~","green"))
+    print(colored("  ~" + "~" * 89 + "~","white"))
+    print(colored("  ~" + todo_five.center(89, " ") + "~","white"))
+    print(colored("  ~" + "~" * 89 + "~","white"))
+    print(" " * 93)
+    print(" " * 93)
+    
+    prompt = "Todo >>"
     file = None
 
     @docopt_cmd
     def do_todo(self, arg):
         """Usage: todo <taskname>"""
-        kanban.add_new_task(arg['<taskname>'])
+        print(kanban.add_new_task(arg['<taskname>']))
 
     def do_todo_list(self, arg):
         """Usage: todo_list"""
-        kanban.view_tasks_todo() 
+        print(kanban.view_tasks_todo()) 
 
     def do_todo_doing(self, arg):
-    	"""Usage: todo_doing <task_id>"""
-    	kanban.move_todo_task_to_doing(arg) 
+    	"""Usage: doing <task_id>"""
+    	print(kanban.move_todo_task_to_doing(arg)) 
     	
     def do_doing_list(self, arg):
     	"""Usage: doing_list"""
-    	kanban.view_tasks_doing()
+    	print(kanban.view_tasks_doing())
 
     def do_doing_done(self, arg):
         """Usage: done <task_id>"""
-        kanban.move_doing_task_to_done(arg)
+        print(kanban.move_doing_task_to_done(arg))
 
     def do_done_list(self, arg):
-        """Usage: list_done"""
-        kanban.view_tasks_done()
+        """Usage: done_list"""
+        print(kanban.view_tasks_done())
 
     def do_list_all(self, arg):
         """Usage: list_all"""
-        kanban.view_all_tasks()     
+        print(kanban.view_all_tasks())
+
+    def do_sync_tasks(self,arg):
+        """sync_tasks"""
+        print(kanban.sync_tasks())         
 
     @docopt_cmd
     def do_serial(self, arg):
@@ -105,6 +144,8 @@ Options:
 
         print('Good Bye!')
         exit()
+
+
 
 opt = docopt(__doc__, sys.argv[1:])
 
