a5c5d6df6f1406102ed5e8429596b85d6aeda296
Flevian <fleviankanaiza@gmail.com>
2017-03-16 18:23:54 +0300

    correcting spellings and spacings

--- kanban.py
+++ kanban.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from kanbanDb import KanbanDb, Base
 from tabulate import tabulate 
 from datetime import datetime
+#from firebase import firebase
 import json
 
 class Kanban():
@@ -24,24 +25,26 @@ class Kanban():
         self.session.add(new_task)
         try:
             self.session.commit()
-            return "Task added successifully"
+            return "Task added"
         except:
             self.session.rollback()
-            return "Task added unsuccessifully"
+            return "Task not added"
 
     def view_tasks_todo(self):
         """View a list of all ToDo tasks"""
-        todo_str = "Done tasks list"
+        todo_str = "Todo tasks list"
         print(todo_str.center(29, "*"))
         todo_tasks = self.session.query(KanbanDb).filter(KanbanDb.task_status == "TODO")
-        todo_list = []
-        for task in todo_tasks:
-            todo_list.append([task.id, task.task_name])
-        return(tabulate(todo_list, ["Task Id", "Task Description"], "fancy_grid"))    
+        if todo_tasks.all():
+            todo_list = []
+            for task in todo_tasks:
+                todo_list.append([task.id, task.task_name])
+            return(tabulate(todo_list, ["Task Id", "Task Description"], "fancy_grid"))
+        else:
+            return "No todo task"        
 
     def move_todo_task_to_doing(self, task_id ):
         """Changing the status of todo task to doing"""
-        start_time = datetime.now()
         todo_doing_task = self.session.query(KanbanDb).filter(KanbanDb.task_status == "TODO")
         if todo_doing_task.all():
             for task in todo_doing_task:
@@ -49,8 +52,12 @@ class Kanban():
                 todo_doing = self.session.query(KanbanDb).filter(KanbanDb.id == task_id).filter(KanbanDb.task_status == "TODO")
                 if todo_doing.all():
                     todo_doing.update({"task_status": "DOING", "start_time": start_time})
-                    self.session.commit()
-                    return "Task moved from doing to done successifully"
+                    try:
+                        self.session.commit()
+                        return "Task moved from doing to done"
+                    except:
+                        self.session.rollback()
+                        return "Task moved"
                 else:
                     return "Task id provided is not for a todo task"
         else:
@@ -58,28 +65,35 @@ class Kanban():
 
     def view_tasks_doing(self):
         """View a list of all Doing tasks"""
-        doing_str = "Done tasks list"
+        doing_str = "Doing tasks list"
         print(doing_str.center(29, "*"))
         doing_tasks = self.session.query(KanbanDb).filter(KanbanDb.task_status == "DOING")
-        doing_list = []
-        for task in doing_tasks: 
-            doing_task_time_taken = datetime.now()  - task.start_time
-            doing_list.append([task.id, task.task_name, doing_task_time_taken])
-        return(tabulate(doing_list, ["Task Id", "Task Description", "Task Duration"], "fancy_grid"))
+        if doing_tasks.all():
+            doing_task_list = []
+            for task in doing_tasks: 
+                doing_task_time_taken = datetime.now()  - task.start_time
+                doing_task_list.append([task.id, task.task_name, doing_task_time_taken])
+            return(tabulate(doing_task_list, ["Task Id", "Task Description", "Task Duration"], "fancy_grid"))
+        else:
+            return "No doing task"
 
     def move_doing_task_to_done(self, task_id):
         """Changing the status of doing task to done"""  
-        done_task = self.session.query(KanbanDb).filter(KanbanDb.task_status == "DOING")
-        if done_task.all():
-            for task in done_task:
+        doing_task = self.session.query(KanbanDb).filter(KanbanDb.task_status == "DOING")
+        if doing_task.all():
+            for task in doing_task:
                 finish_time = str(datetime.now() - task.start_time)
                 doing_done = self.session.query(KanbanDb).filter(KanbanDb.id == task_id).filter(KanbanDb.task_status == "DOING")
                 if doing_done.all():
                     doing_done.update({"task_status": "DONE", "task_duration": finish_time })
-                    self.session.commit()
-                    return "Task moved from doing to done successifully"
+                    try:
+                        self.session.commit()
+                        return "Task moved from doing to done"
+                    except:
+                        self.session.rollback()
+                        return "Task not moved"
                 else:
-                    return "Task id provided is fnot for a doing task"
+                    return "Task id provided is not for a doing task"
         else:
             return "Task id provided does not exist"                        
 
@@ -87,40 +101,45 @@ class Kanban():
         """View a list of all Done tasks"""
         done_str = "Done tasks list"
         print(done_str.center(29, "*"))
-        done_tasks = self.session.query (KanbanDb).filter(KanbanDb.task_status == "DONE") # Get a list of all done tasks
-        done_list = []
-        for task in done_tasks:
-            done_list.append([task.id, task.task_name, task.task_duration])
-        return(tabulate(done_list, ["Task Id", "Task Description", "Task Duration"], "fancy_grid"))    
+        done_tasks = self.session.query(KanbanDb).filter(KanbanDb.task_status == "DONE") # Get a object of all done tasks
+        if done_tasks.all():
+            done_task_list = []
+            for task in done_tasks:
+                done_task_list.append([task.id, task.task_name, task.task_duration])
+            return(tabulate(done_task_list, ["Task Id", "Task Description", "Task Duration"], "fancy_grid"))
+        else:        
+            return "No done task"
 
     def view_all_tasks(self):
         """View a list of all Doing tasks"""
-        todo_list= []
+        """View a list of all Done tasks"""
+        done_str = "All tasks"
+        print(done_str.center(29, "*"))
+        todo_list2= []
         doing_list = []
         done_list =[]
         all_task_dict ={}
-        all_task = []
         done_task = self.session.query(KanbanDb).all()
         if len(done_task)  != 0:
             for task in done_task:
                 if task.task_status == "TODO":
-                    todo_list.append(task.task_name)
+                    todo_list2.append(task.task_name)
                 elif task.task_status == "DOING":
                     doing_list.append(task.task_name)
                 else:
                     done_list.append(task.task_name)
-            all_task_dict = {"TODO" : todo_list, "DOING" :  doing_list, "DONE" : done_list}
+            all_task_dict = {"TODO" : todo_list2, "DOING" :  doing_list, "DONE" : done_list}
             return(tabulate(all_task_dict, headers = "keys", tablefmt = "fancy_grid"))
         else:
             return "No tasks available"       
 
-    def sync_tasks(self):
-        task_backup = self.session.query(KanbanDb).all()
-        firebase = firebase.firebaseApplication("https://kanban-andela.firebaseio.com/")
-        upload_tasks = firebase.post(json.dumps(task_backup))
-        return "tasks sync successifully"    
+    # def sync_tasks(self):
+    #     task_backup = self.session.query(KanbanDb).all()
+    #     firebase = firebase.firebaseApplication("https://kanban-andela.firebaseio.com/")
+    #     upload_tasks = firebase.post(json.dumps(task_backup))
+    #     return "tasks synced successifully"    
 
-def main():
-    pass      
+    def main():
+        pass      
 
 if __name__ == '__main__':main()
